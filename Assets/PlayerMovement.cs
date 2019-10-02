a9190930cde2b9dd912e9d9c52674a33f0a56d97
John Knoop <john.r.knoop@gmail.com>
2019-10-02 14:22:03 -0400

    Added files from Tristen

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    // Update is called once per frame
+    public
+
+
+    void Update()
+    {
+        transform.Translate(Input.GetAxis("Horizontal"), 0f, 0f);
+
+        //Flip the character :
+        Vector3 characterScale = transform.localScale;
+        if (Input.GetAxis("Horizontal") < 0)
+        {
+            characterScale.x = -1;
+        }
+        if (Input.GetAxis("Horizontal") > 0)
+        {
+            characterScale.x = 1;
+        }
+        transform.localScale = characterScale;
+
+    }
+
+    public class Jump : MonoBehaviour
+    {
+
+        Rigidbody rb;
+        void Start()
+        {
+            rb = GetComponent<Rigidbody>();
+        }
+
+        void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                rb.AddForce(new Vector3(0, 10, 0), ForceMode.Impulse);
+            }
+        }
+
+    }
+}
