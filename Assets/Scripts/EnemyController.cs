4047a4368110d42265d4ac19478d17676581c520
John Knoop <john.r.knoop@gmail.com>
2019-10-06 00:49:57 -0400

    Updated comments, updated ALPHA level

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -5,7 +5,7 @@ using UnityEngine.SceneManagement;
 
 public class EnemyController : MonoBehaviour
 {
-
+    //Note this script may be renamed when more enemies are added
     public float moveSpeed;
     public float timeBetweenMovements;
     public float timeToMove;
@@ -40,7 +40,7 @@ public class EnemyController : MonoBehaviour
     {
 
 
-
+        //Creates randomness within for where it goes and when it moves. 
         if (moving)
         {
             timeToMoveCounter -= Time.deltaTime;
@@ -67,7 +67,7 @@ public class EnemyController : MonoBehaviour
                 moveDirection = new Vector3(Random.Range(-1f, 1f) * moveSpeed, (0f) * moveSpeed, 0f);
             }
         }
-
+        //ignore. Old code that may be used for something else
         /**  if (reloading)
           {
               waitReload -= Time.deltaTime;
--- Assets/Scripts/PlayerHealthManager.cs
+++ Assets/Scripts/PlayerHealthManager.cs
@@ -3,8 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class PlayerHealthManager : MonoBehaviour {
-
+public class PlayerHealthManager : MonoBehaviour
+{
+    //This script manages the health of the player
     public int playerMaxHealth;
     public int playerCurrentHealth;
 
@@ -13,26 +14,29 @@ public class PlayerHealthManager : MonoBehaviour {
 
 
     // Use this for initialization
-    void Start () {
+    void Start()
+    {
         playerCurrentHealth = playerMaxHealth;
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //When the player loses all their health the game is reset
         if (playerCurrentHealth <= 0)
         {
             gameObject.SetActive(false);
             SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
-            thePlayer.SetActive(true);
+            gameObject.SetActive(true);
         }
-	}
-
+    }
+    //Calculates the damange the player takes
     public void HurtPlayer(int damageToDeal)
     {
         playerCurrentHealth -= damageToDeal;
     }
-
+    //Sets the player to max health at the start of the game
     public void SetMaxHealth()
     {
         playerCurrentHealth = playerMaxHealth;
