5ae8540267563765f4d915b90a4390c5eca537c6
TBriggz <skaterbriggz@gmail.com>
2019-11-15 05:35:52 -0500

    Transition Into to LVL1
    Added further detail to intro level and ability for the player to teleport from one level to the other.

---
+++ Assets/Scripts/Level1.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Level1 : MonoBehaviour
+{
+    void OnTriggerEnter2D(Collider2D target)
+    {
+
+        if (target.tag == "Player")
+        {
+            SceneManager.LoadScene("Level1");
+
+            //Teleport sound to be added here.
+
+            // play explosion sound
+            //explosionSound.Play();
+            //anim.Play("Destroy");
+
+        }
+
+    }
+
+}
