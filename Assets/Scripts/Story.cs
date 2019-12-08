c6dce53c561ff064df14e42edc48621a2744f592
TBriggz <skaterbriggz@gmail.com>
2019-12-07 23:12:36 -0500

    Scripts now pop up story text

--- Assets/Scripts/Story.cs
+++ Assets/Scripts/Story.cs
@@ -2,25 +2,31 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class StoryPage : MonoBehaviour
+public class Story : MonoBehaviour
 {
     public GameObject story;
-
 
     // Start is called before the first frame update
     void Start()
     {
         story.gameObject.SetActive(false);
     }
-    
-    //Checks if player collided with page from book.
+
+    //Update is called once per frame
+    void Update()
+    {
+        story.gameObject.SetActive(true);
+        //Destroy(gameObject, 10f);
+    }
+
     public void OnCollisionEnter2D(Collision2D collision)
     {
+
         if (collision.gameObject.tag == "Player")
         {
-            Destroy(gameObject);
+            Update();
             story.gameObject.SetActive(true);
-            Destroy(gameObject, 10f);
+            //Destroy(gameObject, 10f);
         }
     }
 }
